e789dbab5c85153af3480b83321ecb903dab7896
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2024-03-14 12:56:20 +0530

    Implement RemainingTime component

--- front-end/polling-system-fe/polling-system-fe/src/components/Poll/Poll.jsx
+++ front-end/polling-system-fe/polling-system-fe/src/components/Poll/Poll.jsx
@@ -11,6 +11,7 @@ const Poll_Container = styled.div`
   width: 50%;
   min-height: 400px;
   height: fit-content;
+  padding-top: 20px;
 
   @media (max-width: 768px) {
     width: 100%;
---
+++ front-end/polling-system-fe/polling-system-fe/src/components/Time/VoteTime.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Typography from '@mui/material/Typography';
+import dayjs from 'dayjs';
+
+const Time_Container = styled.div`
+  width: 100%;
+  min-height: 130px;
+  height: fit-content;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+
+const Remaining_Time = styled.div`
+  display: flex;
+  padding-top: 10px;
+`;
+
+function VoteTime({ targetDate }) {
+  const [remainingTime, setRemainingTime] = useState(calculateRemainingTime());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime(calculateRemainingTime());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  function calculateRemainingTime() {
+    const currentDate = dayjs();
+    const diff = dayjs(targetDate).diff(currentDate, 'second');
+
+    const days = Math.floor(diff / (24 * 60 * 60));
+    const hours = Math.floor((diff % (24 * 60 * 60)) / (60 * 60));
+    const minutes = Math.floor((diff % (60 * 60)) / 60);
+    const seconds = diff % 60;
+
+    return {
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+  return (
+    <Time_Container>
+      <Typography variant="h6" gutterBottom>
+        Choose the most popular house in your school
+      </Typography>
+      <Remaining_Time>
+        <Typography variant="h7" gutterBottom>
+          Remaining Time:
+        </Typography>
+        <Typography
+          sx={{ color: '#000099', marginTop: '-6px', marginLeft: '10px' }}
+          variant="h5"
+          gutterBottom>
+          {String(remainingTime.days).padStart(2, '0')} :{' '}
+          {String(remainingTime.hours).padStart(2, '0')} :{' '}
+          {String(remainingTime.minutes).padStart(2, '0')} :{' '}
+          {String(remainingTime.seconds).padStart(2, '0')}
+        </Typography>
+      </Remaining_Time>
+    </Time_Container>
+  );
+}
+
+export default VoteTime;
