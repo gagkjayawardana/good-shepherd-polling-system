9485ca6bcad011ad3288a023a3b35739212e14e7
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2024-03-14 10:58:35 +0530

    Implement PieChart for display results

---
+++ front-end/polling-system-fe/polling-system-fe/src/components/PieChart/voteData.js
@@ -0,0 +1,124 @@
+// This file will be removed when components are intergrated.
+
+export const VoteData = [
+  {
+    firstName: 'User1',
+    lastName: 'User',
+    email: 'user1@gmail.com',
+    vote: 'Roses'
+  },
+  {
+    firstName: 'User2',
+    lastName: 'User',
+    email: 'user2@gmail.com',
+    vote: 'Violets'
+  },
+  {
+    firstName: 'User3',
+    lastName: 'User',
+    email: 'user3@gmail.com',
+    vote: 'Marguerites'
+  },
+  {
+    firstName: 'User4',
+    lastName: 'User',
+    email: 'user4@gmail.com',
+    vote: 'Lilies'
+  },
+  {
+    firstName: 'User5',
+    lastName: 'User',
+    email: 'user5@gmail.com',
+    vote: 'Roses'
+  },
+  {
+    firstName: 'User6',
+    lastName: 'User',
+    email: 'user6@gmail.com',
+    vote: 'Roses'
+  },
+  {
+    firstName: 'User7',
+    lastName: 'User',
+    email: 'user7@gmail.com',
+    vote: 'Violets'
+  },
+  {
+    firstName: 'User8',
+    lastName: 'User',
+    email: 'user8@gmail.com',
+    vote: 'Marguerites'
+  },
+  {
+    firstName: 'User9',
+    lastName: 'User',
+    email: 'user9@gmail.com',
+    vote: 'Marguerites'
+  },
+  {
+    firstName: 'User10',
+    lastName: 'User',
+    email: 'user10@gmail.com',
+    vote: 'Marguerites'
+  },
+  {
+    firstName: 'User1',
+    lastName: 'User',
+    email: 'user1@gmail.com',
+    vote: 'Roses'
+  },
+  {
+    firstName: 'User2',
+    lastName: 'User',
+    email: 'user2@gmail.com',
+    vote: 'Violets'
+  },
+  {
+    firstName: 'User3',
+    lastName: 'User',
+    email: 'user3@gmail.com',
+    vote: 'Marguerites'
+  },
+  {
+    firstName: 'User4',
+    lastName: 'User',
+    email: 'user4@gmail.com',
+    vote: 'Lilies'
+  },
+  {
+    firstName: 'User5',
+    lastName: 'User',
+    email: 'user5@gmail.com',
+    vote: 'Roses'
+  },
+  {
+    firstName: 'User6',
+    lastName: 'User',
+    email: 'user6@gmail.com',
+    vote: 'Roses'
+  },
+  {
+    firstName: 'User7',
+    lastName: 'User',
+    email: 'user7@gmail.com',
+    vote: 'Violets'
+  },
+  {
+    firstName: 'User8',
+    lastName: 'User',
+    email: 'user8@gmail.com',
+    vote: 'Marguerites'
+  },
+  {
+    firstName: 'User9',
+    lastName: 'User',
+    email: 'user9@gmail.com',
+    vote: 'Marguerites'
+  },
+  {
+    firstName: 'User10',
+    lastName: 'User',
+    email: 'user10@gmail.com',
+    vote: 'Marguerites'
+  }
+];
